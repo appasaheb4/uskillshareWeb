0c9e58bbe94fa94ab566bb9880425867bbea22b1
appasaheb4 <onlyappasaheb4@gmail.com>
2020-02-10 17:17:19 +0530

    contact self msg change.

--- src/screen/Contact/Contact.tsx
+++ src/screen/Contact/Contact.tsx
@@ -95,7 +95,7 @@ export default function Contact( props ) {
                                 src={ require( "../../assets/images/appasaheb.jpeg" ) }
                             /><br /><br />
                             <h6 style={ { fontFamily: fonts.Maquire } }>{
-                                `Hi i'm react and ios native app developer.
+                                `Hi i'm react,nodejs developer.
                                 I live in Pune, India.
                                 My passion is learn new technologies.`
                             }</h6>
